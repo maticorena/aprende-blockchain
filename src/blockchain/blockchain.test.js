677d069b5779aac54046e858da5e96c5fdb98319
maticorena <maticorena@gmail.com>
2023-03-07 18:41:43 -0500

    03-06

--- src/blockchain/blockchain.test.js
+++ src/blockchain/blockchain.test.js
@@ -3,12 +3,14 @@ import Block from './block';
 
 describe('Blockchain', () => {
   let blockchain;
+  let blockchainB;
 
   beforeEach(() => {
     blockchain = new Blockchain();
+    blockchainB = new Blockchain();
   });
 
-  it('every blockchain tiene un bloque genesis', () => {
+  it('every blockchain has a genesis blockchain', () => {
     const [genesisBlock] = blockchain.blocks;
 
     expect(genesisBlock).toEqual(Block.genesis);
@@ -22,5 +24,29 @@ describe('Blockchain', () => {
     const [, lastBlock] = blockchain.blocks;
     expect(lastBlock.data).toEqual(data);
     expect(blockchain.blocks.length).toEqual(2);
+  });
+
+  it('replaces the chain with a valid chain', () => {
+    blockchainB.addBlock('bl4ck-1');
+    blockchain.replace(blockchainB.blocks);
+
+    expect(blockchain.blocks).toEqual(blockchainB.blocks);
+  });
+
+  it('does not replace the chain with one with less blocks', () => {
+    blockchain.addBlock('block-1');
+
+    expect(() => {
+      blockchain.replace(blockchainB.blocks);
+    }).toThrowError('Received chain is not longer than current chain.');
+  });
+
+  it('not replace the chain with one is not valid', () => {
+    blockchainB.addBlock('block-1');
+    blockchainB.blocks[1].data = 'block-h4ck';
+
+    expect(() => {
+      blockchain.replace(blockchainB.blocks);
+    }).toThrowError('Received chain is invalid');
   });
 });
